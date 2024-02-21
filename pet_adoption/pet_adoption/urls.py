c6845954eba0f5f3cc0615af9540d29cb95df1cd
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-02-21 20:21:22 +0530

    set up the urls.py file

--- pet_adoption/pet_adoption/urls.py
+++ pet_adoption/pet_adoption/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('users/', include('users.urls')),
-    path('pets/', include('pets.urls')),
+    path('pets/', include('pets.urls')),  # No need for 'pets/' prefix here
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pet_adoption/pets/urls.py
+++ pet_adoption/pets/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pets/', views.pet_list, name='pet_list'),
+    path('', views.pet_list, name='pet_list'),
 ]

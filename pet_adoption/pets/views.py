d9a75838e820ed7c5572e06f88883655413d08a7
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-02-29 01:12:58 +0530

    re-working on pets app

--- pet_adoption/pets/views.py
+++ pet_adoption/pets/views.py
@@ -9,19 +9,19 @@ from django.shortcuts import render, get_object_or_404,redirect
 
 def adoption_submit(request):
     if request.method == 'POST':
-        # Retrieve form data
+        
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-        # Save the form data to the database
+        
         application = AdoptionApplication(name=name, email=email, message=message)
         application.save()
 
-        # Redirect to a success page or render a success message
+        
         return HttpResponse("Form submitted successfully. Thank you!")
     else:
-        # If the request method is not POST, render the form page
+        
         return render(request, 'adoption_process.html')
 def lost_found_pets(request):
     # Retrieve all lost items from the database
@@ -35,7 +35,7 @@ def pet_list(request):
     return render(request, 'pets/pet_list.html', {'pets': pets})
 
 def pet_detail(request, pk):
-    # Retrieve a specific pet based on its primary key (pk)
+    # Retrieve specific pet based on  primary key (pk)
     pet = get_object_or_404(Pet, pk=pk)
     return render(request, 'pets/pet_detail.html', {'pet': pet})
 
@@ -53,12 +53,12 @@ def adoption_process(request):
         # Redirect to a success page or render a success message
         return HttpResponse("Form submitted successfully. Thank you!")
     else:
-        # If the request method is not POST, render the form page
+        # If request method is not POST, render the form page
         return render(request, 'adoption_process.html')
 
 
 def event_details(request, event_id):
-    # Logic to fetch details of a specific event based on event_id
+    # fetch details of a specific event based on event_id
     event = get_object_or_404(Pet, pk=event_id)
     return render(request, 'pets/event_details.html', {'event': event})
 
